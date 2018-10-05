6679131f619c3ecdac7410c511cb71f8c2626818
njucjc <njucjc@gmail.com>
2018-10-05 12:18:23 +0800

    init commit for proj_02

---
+++ Proj_02/src/main/java/cn/edu/nju/Main.java
@@ -0,0 +1,13 @@
+package cn.edu.nju;
+
+public class Main {
+	public static void main(String [] args) {
+		if(args.length >= 1) {
+			
+		}
+		else {
+			System.out.println("Usage: java Main dict.txt");
+		}
+
+	}
+}
