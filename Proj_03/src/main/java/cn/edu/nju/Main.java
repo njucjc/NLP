0810a57144a7b296111a2fcec52805cc8efa3eb6
njucjc <njucjc@gmail.com>
2018-10-12 20:01:22 +0800

    second version

--- Proj_03/src/main/java/cn/edu/nju/Main.java
+++ Proj_03/src/main/java/cn/edu/nju/Main.java
@@ -7,27 +7,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        if(args.length == 3) {
-//
-//            ChartParser chartParser = new ChartParser(args[0], args[1], args[2]);
-//
-//            do {
-//                Scanner input = new Scanner(System.in);
-//                String s = input.nextLine();
-//                if("exit()".equals(s)) {
-//                    break;
-//                }
-//
-//                chartParser.parsing(s);
-//
-//            }while (true);
-//        }
-//        else {
-//            System.out.println("Usage: java Main dict.txt irregular_verb.txt rule.txt");
-//        }
+        if(args.length == 3) {
 
-        ChartParser chartParser = new ChartParser(args[0], args[1], args[2]);
-        chartParser.parsing("the cat caught a mouse");
+            ChartParser chartParser = new ChartParser(args[0], args[1], args[2]);
+
+            do {
+                Scanner input = new Scanner(System.in);
+                String s = input.nextLine();
+                if("exit()".equals(s)) {
+                    break;
+                }
+
+                chartParser.parsing(s);
+
+            }while (true);
+        }
+        else {
+            System.out.println("Usage: java Main dict.txt irregular_verb.txt rule.txt");
+        }
+
+//        ChartParser chartParser = new ChartParser(args[0], args[1], args[2]);
+//        chartParser.parsing("the cat caught a mouse");
 
         
     }
