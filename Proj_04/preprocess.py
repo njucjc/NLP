1e33b055e3f5c363f8648efde653e2b14cd9c44b
Jim Chen <njucjc@gmail.com>
2018-11-27 20:46:29 +0800

    fix bug.

--- Proj_04/preprocess.py
+++ Proj_04/preprocess.py
@@ -4,6 +4,7 @@ from loader import load_file_content, dump_to_file
 train = load_file_content('data/original/train.txt')
 dev = load_file_content('data/original/dev.txt')
 train_data = encode(train)
+dev_data = encode(dev)
 
 dump_to_file(train_data, 'data/example.train', 'w')
 dump_to_file(dev_data, 'data/example.dev', 'w')
